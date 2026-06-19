28e09a624017fa57a4635e6fecd034902561f4e3
DurgeshPatil13 <durgeshpatil0513@gmail.com>
2026-06-19 15:42:11 +0530

    started making it functional

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-
+// dark mode
 const darkmode = document.querySelector(".darkmodebtn");
 
 darkmode?.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
     console.log("clicked");
 });
+
+// selector
+const totaltask =document.querySelector("#totaltasks");
+const completedtasks=document.querySelector("#completed");
+const pending=document.querySelector("#pendingtasks");
+const completepercent=document.querySelector("#completionper");
+const taskinput=document.querySelector("#taskinput");
+const taskcategory=document.querySelector("#taskcat");
+const tasklevel=document.querySelector("#tasklvl");
+const taskaddbtn=document.querySelector("#taskadd");
+const filterbtnall=document.querySelector("#filterall");
+const filterbtncom=document.querySelector("#filtercomplete");
+const taskcontain=document.querySelector("#taskcontainer");
+const tasktag1=document.querySelector("#extrainfo1");
+const tasktag2=document.querySelector("#extrainfo2");
+const taskeditbtn=document.querySelector("#editbtn");
+const taskdeletebtn=document.querySelector("#deletebtn");
+const progressbar=document.querySelector("#probar");
+const belowprogressinfo=document.querySelector("#belowprobar");
+const progressbarpercentage=document.querySelector("#belowprobarpercentage");
+const studytaskno=document.querySelector("#studytask");
+const worktaskno=document.querySelector("#worktask");
+const personaltaskno=document.querySelector("#personaltask");
+
 
